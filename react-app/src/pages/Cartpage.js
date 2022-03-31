4ffde5be1d2583d3b121893cd493fc460008d1ea
harshith1860 <harshith1860@gmail.com>
2022-03-31 11:25:05 +0530

    Revert "this includes cart"
    This reverts commit 05d7e2b4e5b766ec3c150dc43d1cb120717e5609.

--- react-app/src/pages/Cartpage.js
+++ react-app/src/pages/Cartpage.js
@@ -1,43 +1,12 @@
 import React from 'react'
-import { FaTrash } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
 import Layout from '../components/Layout'
 
-
 function Cartpage() {
-
-  const {cartItems} = useSelector(state=>state.cartReducer);
-
   return (
     <Layout>
-        <table className='table mt-2'>
-
-         <thread>
-         <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Quantity</th>
-            <th>action</th>
-          </tr> 
-         </thread>
-
-         <tbody>
-           
-           {cartItems.map(item=>{
-          
-          return <tr>
-          <td><img src={item.imageURL} height="40" width="40"/></td>
-          <td>{item.name}</td>
-          <td>{FaTrash}</td>
-          </tr>
-
-           }
-           )
-          }
-         </tbody>
-          </table>
+        <h1>cart</h1>
     </Layout>
   )
 }
 
-export default Cartpage;
+export default Cartpage

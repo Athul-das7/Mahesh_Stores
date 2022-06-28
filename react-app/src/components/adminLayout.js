f31033c23455dd7776388c797b93ed85cca4a219
Athul Das <75476124+Athul-das7@users.noreply.github.com>
2022-06-28 23:26:56 +0530

    Update adminLayout.js

--- react-app/src/components/adminLayout.js
+++ react-app/src/components/adminLayout.js
@@ -8,6 +8,7 @@ import {auth} from '../fireconfig'
 import { signOut } from 'firebase/auth';
 import {AuthProvider} from '../contexts/AuthContext'
 
+
 function AdminLayout(props) {
     const authentication = useAuth()
     function handleLogout(){
